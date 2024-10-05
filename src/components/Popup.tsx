cbf37a50016cb140f9bb2413dd55ad994e45b976
fiahfy <d.fiahfy@gmail.com>
2024-10-05 13:14:58 +0900

    Move StoreProvider

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -6,7 +6,7 @@ import {
   Switch,
 } from '@mui/material'
 import type { ChangeEvent } from 'react'
-import { StoreProvider } from '~/contexts/StoreContext'
+import StoreProvider from '~/providers/StoreProvider'
 import { useAppDispatch, useAppSelector } from '~/store'
 import {
   selectChannelPageEnabled,
